8c803e7777477d44ea6455b805b2f80e9d8a9c87
Jednorekibandyta <146111840+Jednorekibandyta@users.noreply.github.com>
2025-04-11 14:19:16 +0200

    main html structure

--- src/index.php
+++ src/index.php
@@ -1,1 +1,80 @@
-<?php
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="keywords" content="koty, kotki"/> 
+    <meta name="destription" content="ta strona pokaże ci ciekawy i intrygujący świat kotów"/>
+    <meta name="author" content="Same sigmy team"/>
+    <meta name="robots" content="none"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <header>
+        <div>
+            <img src="Szescian.png" alt="logo mózgu">
+            <h2>Kto Pytał</h2>
+        </div>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Create Quizz</li>
+                <li>Explore</li>
+                <li>Profile</li>
+            </ul>
+        </nav>
+        <button>Sign In</button>
+    </header>
+    <main>
+        <section id="">
+            <div>
+                <h1>Create and Share Interactive Quizzes</h1>
+                <p>Make learning fun and engaging with our easy-to-use quiz platform. Perfect for educators, trainers, and quiz enthusiasts.</p>
+                <div>
+                    <button>Create Quiz</button>
+                    <button>Take Quiz</button>
+                </div>
+            </div>
+        </section>
+        <section>
+            <h3>Popular Today</h3>
+            <article></article>
+        </section>
+    </main>
+    <footer>
+        <div>
+            <div>
+                <h4></h4>
+                <p>Making quiz creation and sharing easier than ever.</p>
+            </div>
+            <div>
+                <h4>Quick Links</h4>
+                <ul>
+                    <li>About Us</li>
+                    <li>Features</li>
+                    <li>Pricing</li>
+                    <li>Blog</li>
+                </ul>
+            </div>
+            <div>
+                <h4>Support</h4>
+                <ul>
+                    <li>Help Center</li>
+                    <li>Contact Us</li>
+                    <li>Privacy Policy</li>
+                    <li>Terms of Service</li>
+                </ul>
+            </div>
+            <div>
+                <h4>Follow Us</h4>
+                <img src="" alt="logo facebook">
+                <img src="" alt="logo x">
+                <img src="" alt="logo instagram">
+                <img src="" alt="logo linked in">
+            </div>
+        </div>
+        <p>&copy; 2025  Kto Pytał. All rights reserved.</p>
+    </footer>
+</body>
+</html>
+
